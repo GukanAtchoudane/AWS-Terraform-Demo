6efe7eb2a4a530338a63dd74532170f991143a13
root <root@ip-172-31-89-4.ec2.internal>
2026-09-08 14:17:06 +0000

    Just Made Some Changes

--- Terraform-Module/dev-infra/main.tf
+++ Terraform-Module/dev-infra/main.tf
@@ -1,25 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
+module "my-region" {
+  source = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//Terraform-Module/My-Region_module?ref=main"
 
-module "ec2_Dev" {
-  source       = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//Terraform-Module/Ec2-Module?ref=main"
-  instancename = "Dev-Server"
+  myregion     = "us-west-2"
   instancetype = "t2.micro"
-}
-
-module "ec2_Test" {
-  source       = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//Terraform-Module/Ec2-Module?ref=main"
-  instancename = "Test-Server"
-  instancetype = "t2.micro"
-}
-
-module "sg-dev" {
-  source = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//Terraform-Module/SG-Module?ref=main"
-  port   = 8989
-}
-
-module "sg-test" {
-  source = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//Terraform-Module/SG-Module?ref=main"
-  port   = 7979
+  instancename = "us-west-Server"
+  port         = 7979
 }
