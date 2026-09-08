2b6b4a8ce25f20c7e79e4d5b7f33604c3c5edcb8
root <root@ip-172-31-89-4.ec2.internal>
2026-09-08 14:04:48 +0000

    Add Region module

--- Terraform-Module/My-Region_module/main.tf
+++ Terraform-Module/My-Region_module/main.tf
@@ -1,5 +1,5 @@
 module "my-region" {
-    source = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//Region-Module"
+    source = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//My-Region_module"
     myregion = "us-west-2"
     instancetype = "t2.micro"
     instancename = "us-west-Server"
