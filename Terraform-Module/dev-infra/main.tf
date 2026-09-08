1dca13e9da1e5df9f7292f98ef9060ab48a51147
root <root@ip-172-31-89-4.ec2.internal>
2026-09-08 14:24:45 +0000

    Some Changes

--- Terraform-Module/dev-infra/main.tf
+++ Terraform-Module/dev-infra/main.tf
@@ -1,7 +1,7 @@
 module "my-region" {
   source = "git::https://github.com/GukanAtchoudane/AWS-Terraform-Demo.git//Terraform-Module/My-Region_module?ref=main"
 
-  myregion     = "us-west-2"
+  myregion     = "us-east-1"
   instancetype = "t2.micro"
   instancename = "us-west-Server"
   port         = 7979
